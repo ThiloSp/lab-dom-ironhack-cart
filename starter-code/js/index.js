2f955d97afd9adb9d8de5d7fbfebd90a832efc42
Thilo Sperber <t.sperber@gmx.net>
2019-03-16 11:50:48 +0100

    complete second iteration

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -8,14 +8,19 @@ function getTotalPrice() {
   let priceUnit = document.getElementsByClassName("unitPrice");
   let qty = document.getElementsByClassName("input");
   let price = document.getElementsByClassName("total");
-  console.log("priceUnit: ", priceUnit);
+  let totalPrice = document.getElementsByClassName("totalCart");
+  let sum = 0;
+  /* console.log("priceUnit: ", priceUnit);
   console.log("qty: ", qty);
   console.log("price[0]: ", price[0]);
+  console.log("totalPrice[0]: ", totalPrice[0]); */
 
   for (let i = 0; i < priceUnit.length; i++) {
     price[i].innerHTML = `$${priceUnit[i].innerHTML.replace("$", "") *
       qty[i].value}`;
+    sum += priceUnit[i].innerHTML.replace("$", "") * qty[i].value;
   }
+  totalPrice[0].innerHTML = `$${sum}`;
 }
 
 function createQuantityInput() {}
